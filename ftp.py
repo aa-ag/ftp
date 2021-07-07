a2b9fa0629666a31091fd24da290d4911aeb7db9
aa-ag <aaron.aguerrevere@gmail.com>
2021-07-07 07:35:56 -0500

    added print statement to show changing of working directory once connection and authorization are taken care of

--- ftp.py
+++ ftp.py
@@ -3,10 +3,13 @@ from ftplib import FTP
 
 ############------------ CODE ------------############
 # connect to host using default port
+# https://www.debian.org/
 ftp = FTP('ftp.us.debian.org')
 
 # log into ftp using user authentication
 # user anonymous, passwd anonymous@
 print(ftp.login())
-
 # 230 Login successful.
+
+print(ftp.cwd('debian'))
+# 250 Directory successfully changed.
